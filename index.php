6f52e82596fa0d136f78957436344c8e4a3208ab
Ntsako Ngoveni <100789674+NtsakoNgoveni@users.noreply.github.com>
2024-01-20 14:48:43 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    include("templates/header.php");
+    include("templates/sidePanel.php");
+    include("templates/hero.php");
+    include("templates/info.php");
+    include("templates/advert.php");
+    include("templates/benefits.php");
+?>
+</body>
+</html>
